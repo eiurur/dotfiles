4155079e63cd9fff14ea3ce24293b3c5bd96e576
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2016-07-10 12:00:34 +0000

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
